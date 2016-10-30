f46ec589b7dba8772a91fc7be96cea073408431a
Adrienne Lowe <adriennefriend@gmail.com>
2016-10-30 17:19:48 -0500

    Build out test_user_pressed_return_instead_of_entering_URLs with some monkeypatching.

--- test_validator.py
+++ test_validator.py
@@ -1,10 +1,15 @@
 # tests for validator.py project
 
 import unittest
+from unittest.mock import patch
+from contextlib import redirect_stdout
+from io import StringIO
+
+from validator import main
 
 # Make a Test class; name it the name of the thing you're testing
 # We want to test the entire CLI
-# an acceptance or functional test that goes end to end
+# an Acceptance or functional test that goes end to end
 # Doesn't test one individual function; but rather main()
 
 
@@ -12,8 +17,14 @@ class ValidatorProgramTests(unittest.TestCase):
     # write a test for the empty case
     def test_user_pressed_return_instead_of_entering_URLs(self):
         "Test for when user presses return or enter instead of entering a URL"
-        self.fail("TODO: Finish writing your test")
+        with patch('builtins.input', side_effect=['']):
+            with redirect_stdout(StringIO()) as fake_output:
+                main()
+        self.assertEqual(fake_output.getvalue(), "Wait a sec! I think you forgot to enter something.")
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+# make the tests fail in an aspirational way :D
