dbe7621a587225d577d8b2669f945a2ca33e386c
Adrienne Lowe <adriennefriend@gmail.com>
2016-10-30 18:33:39 -0500

    Delete so many print statements and generally clean up code.

--- validator.py
+++ validator.py
@@ -1,35 +1,25 @@
-"""current working copy
-write a function that returns URLS that are bad
-need to return a list containing info about the URLs and
-the status of each URL
-
-MUST HAVES:
-1) Track changes locally with git
-2) Push code to a public GitHub repo
-3) Include a README with a) instructions for running your code and tests
-4) Functionally, program MUST:
-- accept a list of URLs and return a subset of those links that are either
-written incorrectly or do not return a success status code, along with a
-way to identify what was wrong with each link.
-"""
+# MUST HAVES:
+# 1) Track changes locally with git
+# 2) Push code to a public GitHub repo
+# 3) Include a README with a) instructions for running your code and tests
+# 4) Functionally, program MUST:
+# - accept a list of URLs and return a subset of those links that are either
+# written incorrectly or do not return a success status code, along with a
+# way to identify what was wrong with each link
 
 import requests
 
 
 def print_header():
     # a function that shows a header
-    print("---------------------------------")
-    print("   C'mon let's check some urls   ")
-    print("---------------------------------")
+    print("C'mon let's check some urls")
     print()
 
 
 def get_urls():
     # returns the list of URLs
-    print("Hey, type some comma-separated urls!")
-    url = input("... ")
+    url = input("Hey, type some comma-separated urls! > ")
     urls_entered = url.split(",")
-    print(urls_entered)   # debugging aid for my sake :)
     return urls_entered
 
 
@@ -41,34 +31,16 @@ def get_status_code(url):
         print("Exception! The error is:", blarg)
 
 
-def display_results(collected_urls):
-    # Prints a summary at the bottom
-    # Not strictly necessary, can be commented out
-    print()
-    print("------------------------------------------------------------")
-    print()
-    print("Summary")
-    print()
-    print("What we tried:", collected_urls)
-    print()
-    print("Hope this was helpful!")
-    print()
-
-
 def main():
     print_header()
     urls = get_urls()
-    collected_results = []
     for url in urls:
         print()
         print("Handling", url)
         code = get_status_code(url)
-        collected_results.append(code)
         print()
         print("Return value is:", code)
         # print("return value of get_status_code is:", code)
-    display_results(collected_results)    # not strictly necessary, can be commented out
-    return collected_results              # not strictly necessary, can be commented out
 
 
 if __name__ == '__main__':
