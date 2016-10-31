2334faa60d167c49bd03e644cfe58fecce3455db
Adrienne Lowe <adriennefriend@gmail.com>
2016-10-31 12:49:34 -0500

    Big changes for tests: delete the previous functional tests that addressed an older version of main() and other deleted functionality. Goal to make tests short, descriptive, and clear. Add tests for passing in properly formatted failing URLs and an invalid URL.

--- test_validator.py
+++ test_validator.py
@@ -1,91 +1,41 @@
 # tests for validator.py project
 
 import unittest
-from unittest.mock import patch
-from contextlib import redirect_stdout
-from io import StringIO
-from textwrap import dedent
 
-from validator import main, validate_urls
-
-# Make a Test class; name it the name of the thing you're testing
-# We want to test the entire CLI
-# an Acceptance or functional test that goes end to end
-# Doesn't test one individual function; but rather main()
-
-
-class ValidatorProgramTests(unittest.TestCase):
-    # write a test for the empty case
-    def test_user_entered_letter_a_instead_of_entering_URLs(self):
-        "Test for when user enters the letter a instead of URLs"
-        what_is_printed_to_user = self.run_program(user_entered_input="a")
-        aspirational_printed_output = dedent("""\
-            C'mon let's check some urls
-
-            Hey, type some comma-separated urls! > a
-
-            Handling a
-            Exception! The error is: Invalid URL 'a': No schema supplied. Perhaps you meant http://a?
-
-            Return value is: None\n""")
-        self.assertEqual(what_is_printed_to_user, aspirational_printed_output)
-
-    def test_user_entered_a_well_formed_URL_that_returns_a_bad_status_code(self):
-        "It does what we called it."
-        what_is_printed_to_user = self.run_program(user_entered_input="http://httpbin.org/status/404")
-        aspirational_printed_output = dedent("""\
-            C'mon let's check some urls
-
-            Hey, type some comma-separated urls! > http://httpbin.org/status/404
-
-            Handling http://httpbin.org/status/404
-
-            Return value is: Requested http://httpbin.org/status/404 and got 404\n""")
-        self.assertEqual(what_is_printed_to_user, aspirational_printed_output)
-
-    def run_program(self, user_entered_input):
-        "Run program with user-provided input and return printed output "
-        def fake_input(prompt):
-            print(prompt + user_entered_input)
-            return user_entered_input
-        with patch('builtins.input', fake_input):
-            with redirect_stdout(StringIO()) as fake_output:
-                main()
-        return fake_output.getvalue()
-
-# Realized I was probably going the wrong direction writing a functional test for the whole program.
-# Writing validator.py, I need to think less in terms of CLI user-input and more in terms of writing
-# something that can be easily read and executed by a machine, something more machine-friendly.
-# Then, those tests -- in ValidateURLTests will be easier to write as well.
-# Decided to switch more towards TDD, where I write the test first, based on things tried in the REPL, and
-# only after that, attempt to update the code to reflect this functionality.
+from validator import validate_urls
 
 
 class ValidateUrlsTests(unittest.TestCase):
 
     def test_passed_in_empty_list(self):
-        "Passed in empty list and got empty list right back"
+        """Passed in empty list and got empty list right back"""
         self.assertEqual(validate_urls([]), [])
 
     def test_passed_in_a_single_success_url(self):
-        "Passed in a single successful url and since we don't care about those, doesn't give anything"
+        """Passed in a single successful url and since we don't care about those, doesn't give anything"""
         self.assertEqual(validate_urls(["http://httpbin.org/status/200"]), [])
 
     def test_passed_in_a_single_properly_formatted_failing_url(self):
-        "Passed in a single failing url, successfully"
-        pass   # To-do: fix this test
-        # self.assertEqual(validate_urls(["http://httpbin.org/status/410"]),
-        #                ("http://httpbin.org/status/410", "410: Gone"))
+        """Passed in a single failing url, successfully"""
+        self.assertEqual(validate_urls(["http://httpbin.org/status/410"]),
+                        [("http://httpbin.org/status/410", 410, "Is a non-200 result code")])
 
     def test_passed_in_two_success_urls(self):
-        "Passed in two successful urls and since we don't care about those, doesn't give anything"
+        """Passed in two successful urls and since we don't care about those, doesn't give anything"""
         self.assertEqual(validate_urls(["http://httpbin.org/status/200", "http://httpbin.org/status/200"]), [])
 
     def test_passed_in_two_properly_formatted_failing_urls(self):
-        "This should do what it says"
-        pass   # To-do: write this test
+        self.assertEqual(validate_urls(["http://httpbin.org/status/404", "http://httpbin.org/status/500"]),
+                         [("http://httpbin.org/status/404", 404, "Is a non-200 result code"),
+                          ("http://httpbin.org/status/500", 500, "Is a non-200 result code")])
 
-# How will you know when to stop testing? Ask yo friends in QA
+    def test_passed_in_a_single_invalid_url(self):
+        """Invalid URLs have a special return format"""
+        self.assertEqual(validate_urls(["http://???"]),
+                        [("http://???", None, "Invalid URL 'http://???': No host supplied")])
+
+
+# How will you know when to stop testing? Ask yo friends in QA, keep reading, and play, play, play.
 
 if __name__ == '__main__':
     unittest.main()
